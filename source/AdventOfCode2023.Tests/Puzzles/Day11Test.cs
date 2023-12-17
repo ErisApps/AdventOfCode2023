f5c99a6829866af2a039a57c9caa75bbee879601
Eris <erisapps@outlook.be>
2023-12-17 14:46:08 +0100

    ✨ Completed Day 11

---
+++ source/AdventOfCode2023.Tests/Puzzles/Day11Test.cs
@@ -0,0 +1,13 @@
+using AdventOfCode2023.Puzzles;
+using AdventOfCode2023.Tests.Common;
+
+namespace AdventOfCode2023.Tests.Puzzles;
+
+public class Day11Test : HappyPuzzleTestBase<Day11>
+{
+	protected override string Part1AssetName => "Day11.txt";
+	protected override object Part1ExpectedResult => 374;
+
+	protected override string Part2AssetName => "Day11.txt";
+	protected override object Part2ExpectedResult => 82000210;
+}
